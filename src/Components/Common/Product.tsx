1cf49b5db4693c744e410b74e8f4dc9862beda36
towfiqislambd <towfiqislambd@gmail.com>
2025-08-12 17:07:58 +0600

    added details page

--- src/Components/Common/Product.tsx
+++ src/Components/Common/Product.tsx
@@ -1,11 +1,16 @@
+"use client";
+import "swiper/css";
+import "swiper/css/pagination";
 import Image, { StaticImageData } from "next/image";
 import React from "react";
 import { CartLogoSvg, DollarSvg } from "../Svg/SvgContainer";
 import { FaHeart } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 
 type ProductData = {
   id: number;
-  product_image: string | StaticImageData;
+  product_image: (string | StaticImageData)[];
   product_title: string;
   product_price: string;
   is_wishlist: boolean;
@@ -19,7 +24,7 @@ const Product = ({ product }: ProductProps) => {
   return (
     <div className="rounded-t-lg relative">
       {/* wishlist btn */}
-      <button className="absolute top-4 right-5 size-9 rounded-full border grid place-items-center bg-primary-green cursor-pointer">
+      <button className="absolute z-40 top-4 right-5 size-9 rounded-full border border-gray-300 grid place-items-center bg-primary-green cursor-pointer">
         {product?.is_wishlist ? (
           <FaHeart className="text-lg text-accent-red" />
         ) : (
@@ -27,14 +32,26 @@ const Product = ({ product }: ProductProps) => {
         )}
       </button>
 
-      {/* Product Image */}
-      <figure className="w-full h-[350px] rounded-lg bg-[#ddd]">
-        <Image
-          src={product?.product_image}
-          alt="product image"
-          className="w-full h-full object-cover rounded-lg"
-        />
-      </figure>
+      {/* Product Image Gallery */}
+      <Swiper
+        modules={[Pagination]}
+        spaceBetween={20}
+        pagination={{ clickable: true }}
+        className="product_swiper"
+      >
+        {product?.product_image?.map((img, idx) => (
+          <SwiperSlide key={idx}>
+            <figure className="w-full h-[350px] rounded-lg border border-gray-100 relative">
+              <div className="absolute inset-0 bg-black/20 rounded-lg" />
+              <Image
+                src={img}
+                alt="product image"
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </figure>
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
       {/* Product Name */}
       <h3 className="text-primary-green text-xl font-semibold py-3">
--- src/Components/PageComponents/mainPages/shopDetailsComponents/ShopListing.tsx
+++ src/Components/PageComponents/mainPages/shopDetailsComponents/ShopListing.tsx
@@ -1,29 +1,50 @@
 import Container from "@/Components/Common/Container";
 import React from "react";
-import p1 from "@/Assets/p1.png";
+import p1 from "@/Assets/p1.jpg";
 import Product from "@/Components/Common/Product";
 
 const data = [
   {
     id: 1,
-    product_image: p1,
+    product_image: [p1, p1, p1, p1],
     product_title: "Organic Coconut Soap",
     product_price: "8.50",
     is_wishlist: false,
   },
   {
     id: 2,
-    product_image: p1,
+    product_image: [p1, p1, p1, p1],
     product_title: "Organic Coconut Soap",
     product_price: "8.50",
     is_wishlist: true,
   },
   {
     id: 3,
-    product_image: p1,
+    product_image: [p1, p1, p1, p1],
+    product_title: "Organic Coconut Soap",
+    product_price: "8.50",
+    is_wishlist: true,
+  },
+  {
+    id: 4,
+    product_image: [p1, p1, p1, p1],
     product_title: "Organic Coconut Soap",
     product_price: "8.50",
     is_wishlist: false,
+  },
+  {
+    id: 5,
+    product_image: [p1, p1, p1, p1],
+    product_title: "Organic Coconut Soap",
+    product_price: "8.50",
+    is_wishlist: true,
+  },
+  {
+    id: 6,
+    product_image: [p1, p1, p1, p1],
+    product_title: "Organic Coconut Soap",
+    product_price: "8.50",
+    is_wishlist: true,
   },
 ];
 
@@ -31,7 +52,54 @@ const ShopListing = () => {
   return (
     <section id="Listings">
       <Container>
+        {/* Featured Listings */}
         <h2 className="shop_detailed_heading">Featured Listings</h2>
+        <div className="grid grid-cols-3 gap-7 mb-10">
+          {data?.slice(0, 3)?.map(product => (
+            <Product key={product?.id} product={product} />
+          ))}
+        </div>
+
+        {/* All Listings */}
+        <h2 className="shop_detailed_heading">All Listings</h2>
+        {/* Filtering */}
+        <div className="flex gap-7 items-center mb-7">
+          <div>
+            <h3 className="text-secondary-gray font-semibold mb-1.5">
+              Listing Category
+            </h3>
+            <select className="border w-[192px] rounded-lg px-3 py-3 border-gray-400 outline-none text-secondary-gray">
+              <option value="">Product 1</option>
+              <option value="">Product 2</option>
+              <option value="">Product 3</option>
+              <option value="">Product 4</option>
+            </select>
+          </div>
+
+          <div>
+            <h3 className="text-secondary-gray font-semibold mb-1.5">
+              Product Category
+            </h3>
+            <select className="border w-[192px] rounded-lg px-3 py-3 border-gray-400 outline-none text-secondary-gray">
+              <option value="">All</option>
+              <option value="">Product 2</option>
+              <option value="">Product 3</option>
+              <option value="">Product 4</option>
+            </select>
+          </div>
+
+          <div>
+            <h3 className="text-secondary-gray font-semibold mb-1.5">
+              Sort By
+            </h3>
+            <select className="border w-[192px] rounded-lg px-3 py-3 border-gray-400 outline-none text-secondary-gray">
+              <option value="">Recently added</option>
+              <option value="">Product 2</option>
+              <option value="">Product 3</option>
+              <option value="">Product 4</option>
+            </select>
+          </div>
+        </div>
         <div className="grid grid-cols-3 gap-7">
           {data?.map(product => (
             <Product key={product?.id} product={product} />
