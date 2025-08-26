2bc6247b33c22f78e004e7a79628536665a58fce
fahim-sarker <sarkerfahim599@gmail.com>
2025-08-26 10:07:23 +0600

    add shoplist

--- src/app/dashboard/customer/favorites/page.tsx
+++ src/app/dashboard/customer/favorites/page.tsx
@@ -1,20 +1,39 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { data2 } from "@/Components/Data/data";
 import Product from "@/Components/Common/Product";
 import DashBoardHeader from "@/Components/Common/DashBoardHeader";
 
 const page = () => {
+  const tabs: string[] = ["Follow ShopLists", "WishLists"];
+  const [isActive, setIsActive] = useState("Follow ShopLists");
+
   return (
     <>
       <DashBoardHeader
         heading="Yours favorites"
         placeholder="Search favorites"
       />
-      <div className="grid grid-cols-4 gap-x-6 gap-y-10 mt-10">
-        {data2?.map(product => (
-          <Product key={product?.id} product={product} is_feathered={true} />
+      <div className="flex gap-x-3 items-center mt-5">
+        {tabs.map(tab => (
+          <h3
+            key={tab}
+            onClick={() => setIsActive(tab)}
+            className={`text-[20px] font-bold    shrink-0 cursor-pointer ${
+              isActive === tab ? "text-[#000]" : "text-[#77978F]"
+            }`}
+          >
+            {tab}
+          </h3>
         ))}
       </div>
+      {isActive === "Follow ShopLists" && (
+        <div className="grid grid-cols-4 gap-x-6 gap-y-10 mt-10">
+          {data2?.map(product => (
+            <Product key={product?.id} product={product} is_feathered={true} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
