c52768e3510932e3986dfd3d04d13728f2a406ba
fahim-sarker <sarkerfahim599@gmail.com>
2025-08-26 11:49:37 +0600

    find pro dashboard

--- src/app/dashboard/pro/home/page.tsx
+++ src/app/dashboard/pro/home/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <section className="mb-[1000px]">Dashboard Home</section>;
+  return (
+    <section className="mb-[1000px]">
+
+    </section>
+  )
 };
 
 export default page;
