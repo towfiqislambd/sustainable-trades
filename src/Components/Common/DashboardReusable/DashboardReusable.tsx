495f81f1a16b4b2bd2745bbaee3e8bcd774bdd07
fahim-sarker <sarkerfahim599@gmail.com>
2025-08-26 12:05:19 +0600

    complete another portion

--- src/Components/Common/DashboardReusable/DashboardReusable.tsx
+++ src/Components/Common/DashboardReusable/DashboardReusable.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 
-
 const DashboardReusable = () => {
   return (
     <div>
@@ -17,6 +16,19 @@ const DashboardReusable = () => {
           </div>
           <div className="absolute top-0 right-10 w-[2px] bg-[#274F45] h-[45px]"></div>
         </div>
+      </div>
+      <div className="py-9 flex justify-between items-center">
+        <div className="flex flex-col gap-y-2">
+          <h3 className="text-[24px] font-semibold text-[#13141D] tracking-[2.4px]">
+            Hi Jenn,
+          </h3>
+          <h3 className="text-[24px] font-semibold text-[#13141D] tracking-[2.4px]">
+            Here’s your store: Earth’s Essence
+          </h3>
+        </div>
+        <button className="px-[58px] py-4 rounded-[8px] bg-[#E48872] text-[18px] font-semibold text-[#13141D] cursor-pointer hover:bg-transparent duration-500 ease-in-out border border-[#E48872]">
+          Edit Shop
+        </button>
       </div>
     </div>
   );
