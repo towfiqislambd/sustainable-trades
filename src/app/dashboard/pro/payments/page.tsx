25d4bbc263fa61ef21332805f867c308f2b5abbd
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-01 14:27:56 +0600

    complete payement header

--- src/app/dashboard/pro/payments/page.tsx
+++ src/app/dashboard/pro/payments/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const page = () => {
   return (
-    <div>
-      Payments
+    <div className="flex justify-between items-center">
+      <h2 className="text-[40px] font-lato font-semibold text-[#000]">
+        Orders
+      </h2>
+      <div className="flex gap-x-4 items-center">
+        <div className="relative">
+          <input
+            placeholder="Search Payments"
+            type="search"
+            className="py-[10px] pl-4 outline-0 border border-[#BFBEBE] rounded-[8px] text-[16px] text-[#67645F] font-normal w-[300px]"
+          />
+          <div className="absolute top-4 right-3">
+            <FaSearch />
+          </div>
+          <div className="absolute top-0 right-10 w-[2px] bg-[#BFBEBE] h-[45px]"></div>
+        </div>
+        <button className="cursor-pointer py-3 border px-9 rounded-md text-[#274F45] font-lato font-semibold hover:scale-110 duration-500 ease-in-out">
+          Manage Methods
+        </button>
+      </div>
     </div>
   );
 };
