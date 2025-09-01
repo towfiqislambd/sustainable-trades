a82cbb72d34c78c1353b21a155b4a73d0f4f7f93
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-01 14:54:27 +0600

    cteate tabs for payments

---
+++ src/Components/Common/DashboardReusable/AllPaymentTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllPaymentTable = () => {
+  return (
+    <div>AllPaymentTable</div>
+  )
+}
+
+export default AllPaymentTable
--- src/app/dashboard/pro/payments/page.tsx
+++ src/app/dashboard/pro/payments/page.tsx
@@ -1,33 +1,63 @@
-import React from "react";
+"use client";
+import AllPaymentTable from "@/Components/Common/DashboardReusable/AllPaymentTable";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 const page = () => {
+  const tabs = ["All Payments", "Pending", "Paid", "Failed"];
+  const [isactive, setisActive] = useState("All Payments");
+
   return (
-    <div className="flex justify-between items-center">
-      <h2 className="text-[40px] font-lato font-semibold text-[#000]">
-        Orders
-      </h2>
-      <div className="flex gap-x-4 items-center">
-        <div className="relative">
-          <input
-            placeholder="Search Payments"
-            type="search"
-            className="py-[10px] pl-4 outline-0 border border-[#BFBEBE] rounded-[8px] text-[16px] text-[#67645F] font-normal w-[300px]"
-          />
-          <div className="absolute top-4 right-3">
-            <FaSearch />
+    <>
+      <div className="flex justify-between items-center">
+        <h2 className="text-[40px] font-lato font-semibold text-[#000]">
+          Orders
+        </h2>
+        <div className="flex gap-x-4 items-center">
+          <div className="relative">
+            <input
+              placeholder="Search Payments"
+              type="search"
+              className="py-[10px] pl-4 outline-0 border border-[#BFBEBE] rounded-[8px] text-[16px] text-[#67645F] font-normal w-[300px]"
+            />
+            <div className="absolute top-4 right-3">
+              <FaSearch />
+            </div>
+            <div className="absolute top-0 right-10 w-[2px] bg-[#BFBEBE] h-[45px]"></div>
           </div>
-          <div className="absolute top-0 right-10 w-[2px] bg-[#BFBEBE] h-[45px]"></div>
+          <button
+            className="relative cursor-pointer py-[10px] border px-9 rounded-md text-[#274F45] font-lato font-semibold overflow-hidden
+              hover:scale-110 duration-500 ease-in-out
+              after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0 after:bg-[#274F45] after:transition-all after:duration-500 hover:after:h-full hover:after:left-0 hover:text-white"
+          >
+            <span className="relative z-10">Manage Methods</span>
+          </button>
         </div>
-        <button
-          className="relative cursor-pointer py-[10px] border px-9 rounded-md text-[#274F45] font-lato font-semibold overflow-hidden
-  hover:scale-110 duration-500 ease-in-out
-  after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0 after:bg-[#274F45] after:transition-all after:duration-500 hover:after:h-full hover:after:left-0 hover:text-white"
-        >
-          <span className="relative z-10">Manage Methods</span>
-        </button>
       </div>
-    </div>
+      <div className="mt-10">
+        <ul className="flex gap-x-10">
+          {tabs.map(tab => (
+            <li
+              key={tab}
+              onClick={() => setisActive(tab)}
+              className={`cursor-pointer text-[20px] font-semibold ${
+                isactive === tab
+                  ? "text-[#13141D] border-b-2"
+                  : "text-[#77978F]"
+              }`}
+            >
+              {tab}
+            </li>
+          ))}
+        </ul>
+        <div className="mt-8">
+          {isactive === "All Payments" && <AllPaymentTable />}
+          {isactive === "Pending" && <AllPaymentTable />}
+          {isactive === "Paid" && <AllPaymentTable />}
+          {isactive === "Failed" && <AllPaymentTable />}
+        </div>
+      </div>
+    </>
   );
 };
 
