fc8f3d42052b6c55c48b78e3edf01eac78e52a6a
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-01 10:47:12 +0600

    complete modal

--- src/Components/Modals/EditOrderModal.tsx
+++ src/Components/Modals/EditOrderModal.tsx
@@ -16,7 +16,7 @@ const EditOrderModal: React.FC<OrderNoteProps> = ({
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 bg-opacity-50">
-      <div className="bg-white rounded-lg w-[90%] max-w-md p-6 relative">
+      <div className="bg-white rounded-lg w-full max-w-xl p-6 relative">
         {/* Close Button */}
         <div className="flex justify-between py-3 border-b border-gray-300">
           <h3 className="text-[24px] font-normal text-[#000]">
@@ -30,13 +30,73 @@ const EditOrderModal: React.FC<OrderNoteProps> = ({
           </button>
         </div>
 
-        <form className="space-y-5 mt-5">
+        <form className="space-y-3 mt-5">
           {/* Full Name */}
           <div>
             <p className="form-label font-bold">
               Full Name <span className="text-[#8B200C]">*</span>
             </p>
             <input type="text" className="form-input" placeholder="Amy Woods" />
+          </div>
+          <div>
+            <p className="form-label font-bold">
+              Full Name <span className="text-[#8B200C]">*</span>
+            </p>
+            <select className="form-input">
+              <option value="Country">Country</option>
+              <option value="Bangladesh">Bangladesh</option>
+              <option value="Paris">Paris</option>
+              <option value="Australia">Australia</option>
+            </select>
+          </div>
+          <div>
+            <p className="form-label font-bold">
+              Street Address <span className="text-[#8B200C]">*</span>
+            </p>
+            <input
+              type="text"
+              className="form-input"
+              placeholder="123 Hollywood Avenue"
+            />
+          </div>
+          <div>
+            <p className="form-label font-bold">
+              Apt / Suite / Other (Optional)
+            </p>
+            <input
+              type="text"
+              className="form-input"
+              placeholder="Apt / Suite / Other (Optional)"
+            />
+          </div>
+          <div className="flex justify-between gap-x-10">
+            <div>
+              <p className="form-label font-bold">
+                City <span className="text-[#8B200C]">*</span>
+              </p>
+              <input
+                type="text"
+                className="form-input"
+                placeholder="Los Angeles"
+              />
+            </div>
+            <div>
+              <p className="form-label font-bold">
+                Zip Code <span className="text-[#8B200C]">*</span>
+              </p>
+              <input type="text" className="form-input" placeholder="1205" />
+            </div>
+          </div>
+          <div>
+            <p className="form-label font-bold">
+              State <span className="text-[#8B200C]">*</span>
+            </p>
+            <select className="form-input">
+              <option value="State">State</option>
+              <option value="Bangladesh">Dhaka Highway</option>
+              <option value="Paris">Avenue des Champs-Élysées</option>
+              <option value="Australia">George Street</option>
+            </select>
           </div>
         </form>
         <div className="mt-5">
