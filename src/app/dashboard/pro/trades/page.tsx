1dfe9d13645ff39659dfb4c99b5488473d27411a
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-02 10:21:41 +0600

    create trades tabs

--- src/app/dashboard/pro/trades/page.tsx
+++ src/app/dashboard/pro/trades/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
 const page = () => {
+  const tradetabs = [
+    { label: "Pending", count: 5 },
+    { label: "Sent", count: 3 },
+    { label: "Previous", count: 12 },
+    { label: "Canceled", count: 1 },
+  ];
+  const [isActive, setIsActive] = useState("Pending");
   return (
     <div>
       <div className="flex justify-between items-center">
@@ -16,6 +24,23 @@ const page = () => {
             <FaSearch />
           </div>
           <div className="absolute top-0 right-10 w-[2px] bg-[#BFBEBE] h-[45px]"></div>
+        </div>
+      </div>
+      <div className="mt-14">
+        <div className="">
+          <ul className="flex justify-between px-20">
+            {tradetabs.map(tab => (
+              <li
+                key={tab.label}
+                className={`flex gap-x-3 items-center text-[16px] font-normal text-[#13141D] pb-3 ${isActive === tab.label ? "font-semibold border-b-8 rounded-[8px]" : ""}`}
+              >
+                {tab.label}{" "}
+                <span className="border border-[#13141D] rounded-[8px] px-1 py-[2px]">
+                  {tab.count}
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
