c63d1a698e03bb4a678f6a147e65f2d6aa93df47
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-02 10:03:46 +0600

    create trades page

--- src/app/dashboard/pro/trades/page.tsx
+++ src/app/dashboard/pro/trades/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const page = () => {
   return (
     <div>
-      Trades
+      <div className="flex justify-between items-center">
+        <h3 className="text-[40px] font-semibold text-[#000]">Trades</h3>
+        <div className="relative">
+          <input
+            placeholder="Search..."
+            type="search"
+            className="py-[10px] pl-4 outline-0 border border-[#BFBEBE] rounded-[8px] text-[16px] text-[#67645F] font-normal w-[500px]"
+          />
+          <div className="absolute top-4 right-3">
+            <FaSearch />
+          </div>
+          <div className="absolute top-0 right-10 w-[2px] bg-[#BFBEBE] h-[45px]"></div>
+        </div>
+      </div>
     </div>
   );
 };
