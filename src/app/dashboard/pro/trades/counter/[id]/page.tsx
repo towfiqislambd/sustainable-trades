6dd33221f9a0c65d3ab0b56323e780f8a07e519c
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-03 12:04:00 +0600

    create counter page left side

--- src/app/dashboard/pro/trades/counter/[id]/page.tsx
+++ src/app/dashboard/pro/trades/counter/[id]/page.tsx
@@ -1,29 +1,71 @@
-"use client"
-import { useSearchParams } from 'next/navigation';
-import React from 'react'
-import TradeLayout from '../../TradeLayout';
+"use client";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+import TradeLayout from "../../TradeLayout";
+import { LocationSvg1 } from "@/Components/Svg/SvgContainer";
+import { FaRegStar } from "react-icons/fa";
+import Image from "next/image";
+import DetailsImage from "../../../../../../Assets/e1.jpg";
+
 
 const page = () => {
-    const searchParams = useSearchParams();
-    const tabParam = searchParams.get("tab") as "Pending"
+  const searchParams = useSearchParams();
+  const tabParam = searchParams.get("tab") as "Pending";
   return (
     <div>
       <TradeLayout initialTab={tabParam ?? "Pending"}>
-        <div className="">
-          
+        <div className="my-16">
+          <h3 className="text-[#13141D] font-semibold text-[20px] pb-4">
+            Counter Offer
+          </h3>
+          <div className="py-4 border-t border-b border-[#BFBEBE] flex justify-between">
+            <div className="flex gap-x-10">
+              <Image
+                src={DetailsImage}
+                alt="DetailsImage"
+                height={100}
+                width={100}
+                className="h-[100px] w-[100px] rounded-lg"
+              />
+              <div className="flex flex-col gap-y-1">
+                <h3 className="text-[20px] font-semibold text-[#13141D]">
+                  8oz Watermelon Sustainable Bar Soap
+                </h3>
+                <h4 className="text-[20px] font-normal text-[#4B4A47] flex gap-x-5 items-center">
+                  The Soap Shop
+                  <span className="text-[14px] underline cursor-pointer text-[#A7A39C] font-lato">
+                    View Shop
+                  </span>
+                </h4>
+                <div className="flex gap-x-[2px]">
+                  <FaRegStar className="fill-green-950" />
+                  <FaRegStar className="fill-green-950" />
+                  <FaRegStar className="fill-green-950" />
+                  <FaRegStar className="fill-green-950" />
+                  <FaRegStar className="fill-green-950" />
+                </div>
+                <div className="flex gap-x-2 items-center">
+                  <LocationSvg1 />
+                  <h5 className="text-[14px] underline cursor-pointer text-[#A7A39C] font-lato">
+                    13 mi. away -
+                  </h5>
+                  <h5 className="text-[14px] underline cursor-pointer text-[#A7A39C] font-lato">
+                    Denver, CO
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <h4 className="text-[#4B4A47] font-semibold text-[14px]">Product/Service Trade</h4>
+            </div>
+          </div>
         </div>
-        <div className="">
-
-        </div>
-        <div className="">
-
-        </div>
-        <div className="">
-
-        </div>
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
       </TradeLayout>
     </div>
   );
-}
+};
 
-export default page
+export default page;
