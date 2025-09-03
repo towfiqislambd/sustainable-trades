74683b0b67e4cafd7dacd7fe94e32d78b880f46e
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-03 12:20:42 +0600

    create product for counter

--- src/app/dashboard/pro/trades/counter/[id]/page.tsx
+++ src/app/dashboard/pro/trades/counter/[id]/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import TradeLayout from "../../TradeLayout";
 import { LocationSvg1 } from "@/Components/Svg/SvgContainer";
 import { FaRegStar } from "react-icons/fa";
 import Image from "next/image";
 import DetailsImage from "../../../../../../Assets/e1.jpg";
 
-
 const page = () => {
   const searchParams = useSearchParams();
   const tabParam = searchParams.get("tab") as "Pending";
+  const [count, setCount] = useState(1);
+
+  const handleIncrement = () => {
+    setCount(prev => prev + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(prev => (prev > 1 ? prev - 1 : 1));
+  };
   return (
     <div>
       <TradeLayout initialTab={tabParam ?? "Pending"}>
@@ -56,7 +64,67 @@ const page = () => {
               </div>
             </div>
             <div className="">
-              <h4 className="text-[#4B4A47] font-semibold text-[14px]">Product/Service Trade</h4>
+              <h4 className="text-[#4B4A47] font-semibold text-[14px]">
+                Product/Service Trade
+              </h4>
+              <div className="flex gap-x-5 mt-1">
+                <select
+                  name=""
+                  id=""
+                  className="px-4 py-2 rounded-[10px] border border-[#A7A39C] w-[500px]"
+                >
+                  <option
+                    value="Coconut"
+                    className="font-semibold text-[16px] text-[#13141D]"
+                  >
+                    Coconut
+                  </option>
+                  <option
+                    value="Coconut"
+                    className="font-semibold text-[16px] text-[#13141D]"
+                  >
+                    Coconut Oil
+                  </option>
+                  <option
+                    value="Coconut"
+                    className="font-semibold text-[16px] text-[#13141D]"
+                  >
+                    Organic Soap
+                  </option>
+                  <option
+                    value="Coconut"
+                    className="font-semibold text-[16px] text-[#13141D]"
+                  >
+                    Soap Bar
+                  </option>
+                </select>
+                <div className="px-4 py-2 rounded-[10px] border border-[#A7A39C] flex gap-x-3">
+                  <button
+                    onClick={handleDecrement}
+                    className="font-bold text-[20px] text-[#000] cursor-pointer"
+                  >
+                    -
+                  </button>
+                  <button className="font-bold text-[20px] text-[#000]">
+                    {count}
+                  </button>
+                  <button
+                    onClick={handleIncrement}
+                    className="font-bold text-[20px] text-[#000] cursor-pointer"
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <h5 className="flex gap-x-2 te4xt-[16px] font-semibold text-[#4B4A47] items-center justify-end py-2">
+                Total amount: <span className="text-[20px]">$30 </span>
+              </h5>
+              <div className="flex gap-x-5 items-center justify-end">
+                <h6 className="text-[16px] font-semibold text-[#A7A39C]">+</h6>
+                <p className="text-[16px] font-semibold text-[#A7A39C]">
+                  Add another product/service
+                </p>
+              </div>
             </div>
           </div>
         </div>
