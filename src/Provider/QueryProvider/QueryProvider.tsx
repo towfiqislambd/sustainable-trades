1f62206df71c9a85d72f970bf974b0f2414c62f5
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-04 12:56:47 +0600

    fixed queryprovider alignment

--- src/Provider/QueryProvider/QueryProvider.tsx
+++ src/Provider/QueryProvider/QueryProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function ReactQueryProvider({
   children,
