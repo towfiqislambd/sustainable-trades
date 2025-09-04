d765d89f4f83a11b2b3fe3cbb653b69652c666a8
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-04 10:46:15 +0600

    chnage button colour

--- src/Components/Common/DashboardReusable/TradesTabs.tsx
+++ src/Components/Common/DashboardReusable/TradesTabs.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation"; 
+import { useRouter } from "next/navigation";
 import { FaAnglesRight } from "react-icons/fa6";
 import Image, { StaticImageData } from "next/image";
 import { Reload } from "@/Components/Svg/SvgContainer";
@@ -31,6 +31,26 @@ const actionButtons: Record<TradeRequest["status"], string[]> = {
   Sent: ["Message"],
   Approved: ["Message", "Write A review"],
   Canceled: ["Message"],
+};
+const actionButtonStyles: Record<
+  string,
+  { bg?: string; border?: string; text: string }
+> = {
+  Approve: {
+    bg: "bg-[#274F45]",
+    text: "text-white",
+  },
+  Deny: { border: "border-[#8B200C]", text: "text-[#8B200C]" },
+  Counter: {
+    bg: "bg-[#E48872]",
+    text: "text-black",
+    border: "border-[#E48872]",
+  },
+  Message: { border: "border-gray-200", text: "text-black" },
+  "Write A review": {
+    bg: "#5B867B",
+    text: "text-black",
+  },
 };
 
 const TradesTabs: React.FC<TradesTabsProps> = ({ tradeRequests }) => {
@@ -120,23 +140,34 @@ const TradesTabs: React.FC<TradesTabsProps> = ({ tradeRequests }) => {
           {/* Action Buttons */}
           <div className="flex justify-between items-end">
             <div className="flex gap-x-5 flex-wrap">
-              {actionButtons[trade.status].map((btn, i) => (
-                <button
-                  key={i}
-                  onClick={() => {
-                    if (btn === "Counter") {
-                      router.push(`/dashboard/pro/trades/counter/${trade.id}`);
-                    } else {
-                      console.log(`${btn} clicked for trade ${trade.id}`);
-                    }
-                  }}
-                  className={`relative cursor-pointer py-[10px] border px-4 rounded-md font-lato font-semibold overflow-hidden
-                    hover:scale-110 duration-500 ease-in-out
-                    after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0 after:bg-[#274F45] after:transition-all after:duration-500 hover:after:h-full hover:after:left-0 hover:text-white`}
-                >
-                  <span className="relative z-10">{btn}</span>
-                </button>
-              ))}
+              {actionButtons[trade.status].map((btn, i) => {
+                const style = actionButtonStyles[btn] || {
+                  bg: "bg-gray-200",
+                  border: "border-gray-400",
+                  text: "text-black",
+                };
+
+                return (
+                  <button
+                    key={i}
+                    onClick={() => {
+                      if (btn === "Counter") {
+                        router.push(
+                          `/dashboard/pro/trades/counter/${trade.id}`
+                        );
+                      } else {
+                        console.log(`${btn} clicked for trade ${trade.id}`);
+                      }
+                    }}
+                    className={`relative cursor-pointer py-[10px] border px-4 rounded-md font-lato font-semibold overflow-hidden
+            hover:scale-110 duration-500 ease-in-out
+            ${style.bg || ""} ${style.border || "border-2"} ${style.text}
+            after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0 after:bg-[#274F45] after:transition-all after:duration-500 hover:after:h-full hover:text-white`}
+                  >
+                    <span className="relative z-10">{btn}</span>
+                  </button>
+                );
+              })}
             </div>
             <Link
               href={`/dashboard/pro/trades/${trade.id}?tab=${trade.status}`}
