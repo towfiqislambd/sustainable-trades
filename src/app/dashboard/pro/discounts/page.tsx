5f7026961bf318a9dad10a590090acfc03660d0f
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-04 15:36:48 +0600

    create discount page

--- src/app/dashboard/pro/discounts/page.tsx
+++ src/app/dashboard/pro/discounts/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import { FaAngleLeft } from "react-icons/fa";
 
 const page = () => {
-  return <div>discounts</div>;
+  return (
+    <div>
+      <h2 className="text-[40px] font-semibold text-[#13141D]">
+        Create Discount
+      </h2>
+      <div className="border-b border-gray-300">
+        <h4 className="flex gap-x-1 items-center text-[#13141D] font-normal py-4 cursor-pointer">
+          <FaAngleLeft />
+          Back
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default page;
