6a09c7f7856e61114f55293b7ec0f84c0278737f
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-11 14:38:01 +0600

    initialize header

--- src/app/dashboard/pro/notification/page.tsx
+++ src/app/dashboard/pro/notification/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const page = () => {
   return (
     <>
-      
+      <div className=" bg-[#FFF] rounded-md p-5 w-fit mx-auto"></div>
     </>
   );
 };
