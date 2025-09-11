09bd36ae99ff6f2d81231d2aa082411370c537e1
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-11 12:27:08 +0600

    create right coloumn of view listings

--- src/app/dashboard/pro/view-listing/page.tsx
+++ src/app/dashboard/pro/view-listing/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaPlus } from "react-icons/fa";
 
 const page = () => {
   return (
@@ -29,20 +30,31 @@ const page = () => {
               >
                 Name: A - Z
               </option>
+            </select>
+          </div>
+          <div className="">
+            <p className="text-[#13141D] text-[16px] font-semibold">
+              Listing Status
+            </p>
+            <select
+              name=""
+              id=""
+              className="p-4 rounded-[10px] border border-[#A7A39C] mt-2  w-[190px] cursor-pointer"
+            >
               <option
                 value=""
                 className="text-[#13141D] text-[16px] font-normal"
               >
-                Name: A - Z
-              </option>
-              <option
-                value=""
-                className="text-[#13141D] text-[16px] font-normal"
-              >
-                Name: A - Z
+                All
               </option>
             </select>
           </div>
+        </div>
+        <div className="">
+          <button className="h-[60px] rounded-[8px] bg-[#E48872] text-[16px] font-semibold text-[#13141D] cursor-pointer hover:bg-transparent duration-500 ease-in-out border border-[#E48872] w-[190px] flex gap-x-2 justify-center items-center">
+            <FaPlus />
+            Add New Listing
+          </button>
         </div>
       </div>
     </>
