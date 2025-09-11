2b88517018643162e4c64b9a6568cf8a6567eb91
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-11 14:42:07 +0600

    add heading

--- src/app/dashboard/pro/notification/page.tsx
+++ src/app/dashboard/pro/notification/page.tsx
@@ -3,7 +3,16 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <div className=" bg-[#FFF] rounded-md p-5 w-fit mx-auto"></div>
+      <div className=" bg-[#FFF] rounded-md w-fit mx-auto">
+        <div className="border-b border-[#E5E5E5]">
+          <h3 className="text-[40px] font-semibold text-[#000] flex items-center gap-x-2 p-5">
+            Notifications
+          </h3>
+        </div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
     </>
   );
 };
