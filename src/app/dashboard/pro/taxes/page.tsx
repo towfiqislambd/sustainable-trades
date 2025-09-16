d8c5246f3a32ec11fc103156e63249edb3282468
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-16 15:27:06 +0600

    initialize taxes page

--- src/app/dashboard/pro/taxes/page.tsx
+++ src/app/dashboard/pro/taxes/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { RxCross2 } from "react-icons/rx";
 
 const page = () => {
   return (
     <div className=" flex justify-center items-center h-full">
-      <div className="bg-[#FFFCF9] p-10 shadow-2xl rounded-[20px]"></div>
+      <div className="bg-[#FFFCF9] p-8 shadow-2xl rounded-[20px]">
+        <div className="flex justify-between items-center">
+          <h4 className="text-[24px] text-[#000] font-bold">Add Tax Rate</h4>
+          <RxCross2 className="mt-1 size-5" />
+        </div>
+        <form action="" className="mt-8"></form>
+      </div>
     </div>
   );
 };
