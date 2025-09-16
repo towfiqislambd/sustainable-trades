e147d4a04a3ab292d12a51dd3d220d7194db0107
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-16 15:17:28 +0600

    complete payment method page

--- src/app/dashboard/pro/payment-method/page.tsx
+++ src/app/dashboard/pro/payment-method/page.tsx
@@ -1,3 +1,4 @@
+import { Paypal } from "@/Components/Svg/SvgContainer";
 import React from "react";
 
 const page = () => {
@@ -11,8 +12,14 @@ const page = () => {
         <br /> payments from buyers.
         <span className="underline cursor-pointer">Learnmore</span>.
       </h5>
-      <div className="mt-18">
-        
+      <div className="mt-18 border border-[#BFBEBE] rounded-[10px] p-6 w-full max-w-[480px]">
+        <Paypal />
+        <p className="font-normal text-[16px] text-[#3D4145]  pt-2">
+          Customers can check out from your store with a PayPal or Venmo Account
+        </p>
+        <button className="mt-10 p-3 border border-[#274F45] rounded-md text-[14px] font-semibold text-[#274F45] cursor-pointer hover:text-white hover:bg-[#274F45] duration-500 ease-in-out uppercase">
+          Manage
+        </button>
       </div>
     </div>
   );
