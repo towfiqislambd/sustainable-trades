83d50485387ec26db04850c1c3f49270908f0fd9
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-16 15:10:17 +0600

    add payment method page

---
+++ src/app/dashboard/pro/payment-method/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h2 className="text-[40px] font-lato font-semibold text-[#000]">
+        Store Payments
+      </h2>
+      <h5 className="text-[20px] text-[#3D4145] font-normal pt-4">
+        Connect and manage how you'd like to receive
+        <br /> payments from buyers.
+        <span className="underline cursor-pointer">Learnmore</span>.
+      </h5>
+      <div className="mt-18">
+        
+      </div>
+    </div>
+  );
+};
+
+export default page;
