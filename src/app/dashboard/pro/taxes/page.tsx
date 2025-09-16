4d485cc69af1ef22ca4302b51067bfebb38c54c5
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-16 15:21:11 +0600

    create taxes page

---
+++ src/app/dashboard/pro/taxes/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className=" flex justify-center items-center h-full">
+      <div className="bg-[#FFFCF9] p-10 shadow-2xl rounded-[20px]"></div>
+    </div>
+  );
+};
+
+export default page;
