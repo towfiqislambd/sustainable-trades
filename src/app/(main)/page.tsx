b280117814fab19f3bdbf18f656e49acd93b0bb0
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-18 10:26:51 +0600

    fix the import lines

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,13 @@
-import CommunityMember from "@/Components/PageComponents/mainPages/homePageComponents/CommunityMember";
-import ExploreProduct from "@/Components/PageComponents/mainPages/homePageComponents/ExploreProduct";
-import FeaturedShops from "@/Components/PageComponents/mainPages/homePageComponents/FeaturedShop";
+import { getMissionData, getPricingData } from "@/Hooks/api/cms_api";
+import Pricing  from "@/Components/PageComponents/mainPages/homePageComponents/Pricing";
+import Subscribe from "@/Components/PageComponents/mainPages/homePageComponents/Subscribe";
 import HomeBanner from "@/Components/PageComponents/mainPages/homePageComponents/HomeBanner";
 import HowItWorks from "@/Components/PageComponents/mainPages/homePageComponents/HowItWorks";
-import MagicMarkers from "@/Components/PageComponents/mainPages/homePageComponents/MagicMarkers";
 import OurMission from "@/Components/PageComponents/mainPages/homePageComponents/OurMission";
-import Pricing from "@/Components/PageComponents/mainPages/homePageComponents/Pricing";
-import Subscribe from "@/Components/PageComponents/mainPages/homePageComponents/Subscribe";
-import { getMissionData, getPricingData } from "@/Hooks/api/cms_api";
+import MagicMarkers from "@/Components/PageComponents/mainPages/homePageComponents/MagicMarkers";
+import FeaturedShops from "@/Components/PageComponents/mainPages/homePageComponents/FeaturedShop";
+import ExploreProduct from "@/Components/PageComponents/mainPages/homePageComponents/ExploreProduct";
+import CommunityMember from "@/Components/PageComponents/mainPages/homePageComponents/CommunityMember";
 
 const Page = async () => {
   const missionData = await getMissionData();
