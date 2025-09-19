9c63d1efa9b11316fcf3a455d5552977a4415c25
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-19 14:53:22 +0600

    create basic dashboard listing page

--- src/app/dashboard/basic/listings/page.tsx
+++ src/app/dashboard/basic/listings/page.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const 
- = () => {
+const page = () => {
   return (
     <div>
-      
+      <h3 className="text-[40px] font-semibold text-[#13141D]">
+       Listings
+      </h3>
     </div>
-  )
-}
+  );
+};
 
-export default 
+export default page;
