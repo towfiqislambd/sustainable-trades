cfdce7fb81096fbc402bac6fc4864eeb047a5b2d
fahim-sarker <sarkerfahim599@gmail.com>
2025-09-19 15:30:37 +0600

    create basic dashboard listing page

--- src/app/dashboard/basic/listings/page.tsx
+++ src/app/dashboard/basic/listings/page.tsx
@@ -3,9 +3,28 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h3 className="text-[40px] font-semibold text-[#13141D]">
-       Listings
-      </h3>
+      <h3 className="text-[40px] font-semibold text-[#13141D]">Listings</h3>
+      <div className="flex gap-x-6">
+        <div>
+          <p className="text-[#13141D] text-[16px] font-semibold">Sort by :</p>
+          <select className="p-4 rounded-[10px] border border-[#A7A39C] mt-2 w-[190px] cursor-pointer">
+            <option>Name: A - Z</option>
+            <option>Name: Z - A</option>
+            <option>Price: Low to High</option>
+            <option>Price: High to Low</option>
+          </select>
+        </div>
+        <div>
+          <p className="text-[#13141D] text-[16px] font-semibold">
+            Listing Status
+          </p>
+          <select className="p-4 rounded-[10px] border border-[#A7A39C] mt-2 w-[190px] cursor-pointer">
+            <option>All</option>
+            <option>Active</option>
+            <option>Inactive</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 };
