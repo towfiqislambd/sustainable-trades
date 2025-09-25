cdaad1f48126e6d906cbece880e83e898ede7e41
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2025-09-25 16:32:39 +0600

    member spotlight responsive added

--- src/app/(main)/community-member-spotlight/page.tsx
+++ src/app/(main)/community-member-spotlight/page.tsx
@@ -41,21 +41,24 @@ const Community = () => {
     <section className="py-10">
       <Container>
         <div className="space-y-6">
-          {data?.map(item => (
-            <div key={item?.id} className="rounded-xl flex items-center border border-gray-300">
+          {data?.map((item) => (
+            <div
+              key={item?.id}
+              className="rounded-xl flex flex-col lg:flex-row lg:items-center border border-gray-300"
+            >
               {/* Left - Shop Image */}
-              <figure className="w-[229px] h-[230px] shrink-0 rounded-l-xl relative">
+              <figure className="w-full lg:w-[229px] h-[300px] lg:h-[230px] shrink-0 rounded-l-xl relative">
                 <Image
                   src={item?.shop_image}
                   alt="shop_image"
                   fill
-                  className="w-full h-full rounded-l-xl"
+                  className="w-full h-full rounded-tl-xl rounded-tr-xl lg:rounded-l-xl"
                 />
               </figure>
 
               {/* Right */}
-              <div className="grow px-4 py-2">
-                <div className="flex justify-between items-center">
+              <div className="lg:grow px-4 py-2">
+                <div className="flex flex-col sm:flex-row md:justify-between md:items-center">
                   <h3 className="tracking-[0.2px] font-semibold text-secondary-gray text-xl mb-1">
                     {item?.shop_name}
                   </h3>
@@ -65,13 +68,15 @@ const Community = () => {
                   </p>
                 </div>
 
-                <p className="mb-3 text-gray-500">Owner : {item?.shop_owner}</p>
+                <p className="mb-3 text-[14px] md:text-base text-gray-500">
+                  Owner : {item?.shop_owner}
+                </p>
 
-                <p className="text-gray-600 max-w-[800px] mb-4">
+                <p className="text-gray-600 text-[13px] md:text-base max-w-[800px] mb-4">
                   {item?.description}
                 </p>
 
-                <button className="px-4 py-2 rounded-lg font-semibold flex gap-2 items-center bg-[#D4E2CB] text-primary-green cursor-pointer duration-500 transition-transform hover:scale-105">
+                <button className="px-4 text-[12px]  py-2 rounded-lg font-semibold flex gap-2 items-center bg-[#D4E2CB] text-primary-green cursor-pointer duration-500 transition-transform hover:scale-105">
                   <span>View Shop</span>
                   <span>
                     <RightSvg />
