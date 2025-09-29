cef48efbf7f3adfbe7f0b5dd19bf73fb330e9ea6
towfiqislambd <towfiqislambd@gmail.com>
2025-09-29 16:41:09 +0600

    added basic navbar

--- src/Shared/DashboardHeader.tsx
+++ src/Shared/DashboardHeader.tsx
@@ -212,8 +212,8 @@ const DashboardHeader = ({ setOpen }: DashboardHeaderProps) => {
 
           {user?.role !== "customer" && (
             <Link
-              href="/shop-details/1"
-              className="px-5 py-2 hidden md:block rounded-lg bg-accent-red text-secondary-black cursor-pointer shadow-[0_3px_10px_0_rgba(0,0,0,0.12),_0_3px_8px_0_rgba(0,0,0,0.08)] duration-300 transition-all hover:text-accent-red hover:bg-transparent border border-accent-red hover:scale-95 "
+              href={`/view-my-shop/${user?.shop_info?.id}`}
+              className="px-5 py-2 block rounded-lg bg-accent-red text-secondary-black cursor-pointer shadow-[0_3px_10px_0_rgba(0,0,0,0.12),_0_3px_8px_0_rgba(0,0,0,0.08)] duration-300 transition-all hover:text-accent-red hover:bg-transparent border border-accent-red hover:scale-95 "
             >
               View Shop
             </Link>
