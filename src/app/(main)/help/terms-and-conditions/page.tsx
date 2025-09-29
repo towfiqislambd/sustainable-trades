e9a918701b1af8965e4467374aad255d3e9fdc59
alimuzzaman-zamaddar <alimuzzamanzamaddar@gmail.com>
2025-09-29 16:44:49 +0600

    done

--- src/app/(main)/help/terms-and-conditions/page.tsx
+++ src/app/(main)/help/terms-and-conditions/page.tsx
@@ -9,19 +9,19 @@ const Page = () => {
 
   return (
     <>
-      <section className="mb-40 mt-20">
+      <section className="mb-15 lg:mb-40 mt-10 lg:mt-20">
         <Container>
-          <div className="flex items-start gap-14">
+          <div className="flex flex-col lg:flex-row items-start gap-5 md:gap-14">
             {/* Left - Tabs */}
             <HelpUsTab />
 
             {/* Right */}
-            <div className="grow">
+            <div className="grow px-2.5 sm:px-0">
               <div
                 dangerouslySetInnerHTML={{
                   __html: termsData?.data?.description,
                 }}
-                className="text-secondary-gray leading-8 max-w-[900px] [&_ul]:list-disc [&_ul]:pl-5 [&_ol]:pl-5 [&_ol]:list-decimal"
+                className="text-secondary-gray leading-8 text-sm lg:text-base  xl:max-w-[900px] [&_ul]:list-disc [&_ul]:pl-5 [&_ol]:pl-5 [&_ol]:list-decimal"
               />
             </div>
           </div>
