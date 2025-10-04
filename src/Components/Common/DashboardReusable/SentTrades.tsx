ba689f59aff4fce9dc2f48a9d10e65e000f7a2dc
fahim-sarker <sarkerfahim599@gmail.com>
2025-10-04 17:29:23 +0600

    swithch branch

--- src/Components/Common/DashboardReusable/SentTrades.tsx
+++ src/Components/Common/DashboardReusable/SentTrades.tsx
@@ -1,14 +1,18 @@
+"use client";
 import React from "react";
 import TradesTabs from "./TradesTabs";
 import { tradeRequests } from "@/Components/Data/data";
 
 const SentTrades = () => {
-const senttradedata = tradeRequests.filter(
-  trade =>
-    trade.status === "Pending" ||
-    trade.status === "Approved" ||
-    trade.status === "Canceled" 
-);
+  const senttradedata = tradeRequests.filter(
+    trade =>
+      trade.status === "Pending" ||
+      trade.status === "Approved" ||
+      trade.status === "Canceled"
+  );
+
+  console.log("sent", senttradedata);
+
   return (
     <>
       <TradesTabs tradeRequests={senttradedata} />
