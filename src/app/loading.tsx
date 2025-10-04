42fb61e8ed079ad85d3b644c2e4cb459010712c5
towfiqislambd <towfiqislambd@gmail.com>
2025-10-04 17:45:27 +0600

    doen

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { PuffLoader } from "react-spinners";
 
 const loading = () => {
   return (
     <div className="h-screen flex justify-center items-center text-5xl">
-      Loading.....
+      <PuffLoader color="#274f45" />
     </div>
   );
 };
