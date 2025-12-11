f33565fc7ab7c5f36b549fd4125af20aec23a040
towfiqislambd <towfiqislambd@gmail.com>
2025-12-11 11:29:28 +0600

    fixed 404 isues

--- src/Components/PageComponents/mainPages/shopPageComponents/ProductLocation.tsx
+++ src/Components/PageComponents/mainPages/shopPageComponents/ProductLocation.tsx
@@ -77,6 +77,7 @@ const ProductLocation = () => {
                               className="text-primary-green text-xs md:text-sm"
                             />
                           ))}
+
                           {Array.from({
                             length: 5 - +product?.reviews_avg_rating,
                           }).map((_, index) => (
